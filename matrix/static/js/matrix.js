7acd5ce2f52aa7f993b04714aa7ac421b249a83a
Abi Raja <abimanyuraja@gmail.com>
2013-02-02 12:45:13 -0800

    Support input and display of matrix. Also, added a live properties checker that currently checks if a matrix is the identity.

--- matrix/static/js/matrix.js
+++ matrix/static/js/matrix.js
@@ -3,9 +3,80 @@ $(document).ready(function(){
 });
 
 function renderApp(){
-	$("#app").html("<textarea rows=\"20\" cols=\"20\"></textarea>" 
-    + "<h1>Tester chester</h1>"
-    + "Not so bad eh. Cool it's all working great now!");
+	$("#app").html("" +
+        "<textarea id='input' rows=\"20\" cols=\"20\"></textarea>" +
+        "<div id='matrix'><div>");
+    
+    $('#input').keyup(function(){
+        var input = $('#input').val();
+        var matrix = parseMatrix(input);
+        
+        $('#matrix').html(renderMatrix(matrix));
+        $('#matrix').append($(renderProperties(matrix)));
+    });
+    
+}
+
+function renderMatrix(matrix){
+    var html = "<div>"; 
+    matrix.each(function(row, i){
+        html += "<div>";
+        row.each(function(el, j){
+            html += "<span>(" + i + "," + j + ") " + el + "</span>";
+        });
+       html += "</div>"; 
+    });
+    html += "</div>";
+    console.log(html);
+    return html;
+}
+
+// Iterates through rows, and then each row's columns
+function iterateElements(matrix, fn){
+    matrix.each(function(row, i){
+        row.each(function(el, j){
+            fn(el, i, j);
+        });
+    });
+}
+
+function isIdentity(matrix){
+    var success = true;
+    iterateElements(matrix, function(el, i, j){
+        console.log(el, i, j);
+        if (i == j && el != 1){
+            success = false;
+        }
+        if (i != j && el != 0){
+            success = false;
+        }
+    });
+    return success;
+}
+
+function renderProperties(matrix){
+    var html = "<div>"; 
+    if(isIdentity(matrix)){
+        html += "Identity";
+    }else{
+        html += "Not identity";
+    }
+    html += "</div>";
+    return html;
+}
+
+function parseMatrix(input){
+    var matrix = [];
+    input.split('\n').each(function(line){
+        var row = [];
+        line.split(' ').each(function(el){
+            // Allow real numbers (and even complex numbers?)
+            row.push(parseInt(el)); 
+        });
+        matrix.push(row);
+    });
+    renderMatrix(matrix);
+    return matrix;
 }
 
 renderApp();
