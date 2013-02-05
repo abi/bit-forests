617583dfddba983985165b5eeee965c74ae4fb20
Abi Raja <abimanyuraja@gmail.com>
2013-02-04 23:49:10 -0800

    Added orthogonality of matrix checking.

--- matrix/static/js/matrix.js
+++ matrix/static/js/matrix.js
@@ -44,7 +44,6 @@ function iterateElements(matrix, fn){
 function isIdentity(matrix){
     var success = true;
     iterateElements(matrix, function(el, i, j){
-        console.log(el, i, j);
         if (i == j && el != 1){
             success = false;
         }
@@ -59,12 +58,73 @@ function isUpperTriangular(matrix){
         
 }
 
+function isOrthogonal(matrix){
+    // Check that every row is orthogonal to every other row
+    for(var i = 0; i < matrix.length; i++){
+        // TODO: Out of bounds
+        for(var j = i + 1; j < matrix.length; j++){
+            if(dot(matrix[i], matrix[j]) != 0){
+                return false;
+            }
+        }
+    }
+    
+    var cols = getCols(matrix);
+    for(var i = 0; i < cols.length; i++){
+        // TODO: Out of bounds
+        for(var j = i + 1; j < cols.length; j++){
+            if(dot(cols[i], cols[j]) != 0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function getCols(matrix){
+    // TODO: Bounds checking
+    var cols = [];
+    for(var j = 0; j < matrix[0].length; j++){
+        var col = [];
+        for(var i = 0; i < matrix.length; i++){
+            col.push(matrix[i][j]);
+        }
+        cols.push(col);
+    }
+    console.log(cols);
+    return cols;
+}
+
+function tranpose(vector){
+    // Don't need this for now       
+}
+
+function dot(v1, v2){
+    if(v1.length != v2.length){
+        //alert("BAD BAD BAD!");
+        // TODO: Return a bad a value or throw an exception
+        return;
+    }
+    
+    var sum = 0;
+    for(var i = 0; i < v1.length; i++){
+        sum += v1[i] * v2[i];
+    }
+    return sum;
+}
+
 function renderProperties(matrix){
     var html = "<div>"; 
     if(isIdentity(matrix)){
         html += "Identity";
     }else{
         html += "Not identity";
+    }
+    
+    if(isOrthogonal(matrix)){
+        html += "Orthogonal";
+    }else{
+        html += "Not orthogonal";
     }
     html += "</div>";
     return html;
