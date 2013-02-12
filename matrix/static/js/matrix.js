0ae7f112ad6810cc67fcdf47676608517abf45a2
Abi Raja <abimanyuraja@gmail.com>
2013-02-12 01:09:07 -0800

    Fixed some porting bugs.

--- matrix/static/js/matrix.js
+++ matrix/static/js/matrix.js
@@ -15,9 +15,10 @@ function renderApp(){
     
     $('#input').keyup(function(){
         var input = $('#input').val();
-        var matrix = parseMatrix(input);
+        var matrix = matrixParser(input);
         
-        $('#matrix').html(renderMatrix(matrix));
+        var renderer = new HtmlRenderer(matrix);
+        $('#matrix').html(renderer.html());
         $('#matrix-props').html(renderProperties(matrix));
     });
     
@@ -39,8 +40,7 @@ function renderApp(){
     
     function updateInput(matrix){
         $("#input").val(matrix.toString());
-        // renderer = new HtmlRenderer(matrix);
-        // $("#input").renderer.html();
+
         $("#input").focus();
         $("#input").trigger("keyup");
     }
@@ -57,20 +57,6 @@ function renderMatrix(matrix){
     });
     html += "</div>";
     return html;
-}
-
-
-function isIdentity(matrix){
-    var success = true;
-    iterateElements(matrix, function(el, i, j){
-        if (i == j && el != 1){
-            success = false;
-        }
-        if (i != j && el != 0){
-            success = false;
-        }
-    });
-    return success;
 }
 
 function isOrthogonal(matrix){
@@ -124,27 +110,36 @@ function dot(v1, v2){
 }
 
 function Matrix(rows, cols){
+    if(typeof rows === "undefined")
+        rows = 0;
+
     this.rows = rows;
     this.cols = cols;
 
-    // Square matrices
+    // Default to square matrices
     if(typeof cols == "undefined")
         this.cols = rows;
 
-    // Initialize matrix to zeroes
-    this._storage = [];
-    for(var i = 0; i < rows; i++){
-        var row = [];
-        for(var j = 0; j < cols; j++){
-            row.push(0);
-        }
-        this._storage.push(row);
-    }
+    this.init();
 }
 
 Matrix.prototype = {
+    init: function(){
+        // Initialize matrix to zeroes
+        this._storage = [];
+        for(var i = 0; i < this.rows; i++){
+            var row = [];
+            for(var j = 0; j < this.cols; j++){
+                row.push(0);
+            }
+            this._storage.push(row);
+        }
+    },
     setSize: function(rows, cols){
         // TODO: Truncate existing arrays or add zeroes as necessary
+        this.rows = rows;
+        this.cols = cols;
+        this.init();
     },
     at: function(i, j){
         return this._storage[i][j];
@@ -156,28 +151,28 @@ Matrix.prototype = {
     // Properties 
     isSymmetric: function(){
         var isSymmetric = true;
-        getElementWise(function(i, j, el){
-            if(el !== this._storage[i][j]){
+        var self = this;
+        this.getElementWise(function(i, j, el){
+            if(el !== self.at(j, i)){
                 isSymmetric = false;
             }
         });
         return isSymmetric;
     },
     isIdentity: function(){
-        var success = true;
-        getElementWise(matrix, function(i, j, el){
-            if (i === j && el !== 1){
-                success = false;
-            }
-            if (i !== j && el !== 0){
-                success = false;
-            }
+        var isIdentity = true;
+        this.getElementWise(function(i, j, el){
+            if (i === j && el !== 1)
+                isIdentity = false;
+            if (i !== j && el !== 0)
+                isIdentity = false;
         });
-        return success;
+        return isIdentity;
     },
     getElementWise: function(fn){
         for(var i = 0; i < this.rows; i++){
             for(var j = 0; j < this.cols; j++){
+                // TODO: Call fn with the correct context
                 fn(i, j, this._storage[i][j]);
             }
         }
@@ -204,6 +199,28 @@ Matrix.prototype = {
         return str;
     }
 }
+
+function matrixParser(str){
+    var matrix = [];
+    str.split('\n').each(function(line){
+        line = line.compact();
+        // Ignore any empty new lines
+        if(line.length == 0) return;
+        var row = [];
+        line.split(' ').each(function(el){
+            // Allow real numbers (and even complex numbers?)
+            row.push(parseInt(el)); 
+        });
+        matrix.push(row);
+    });
+    
+    // HACK
+    var mMatrix = new Matrix(matrix.length, matrix[0].length);
+    mMatrix._storage = matrix;
+    
+    return mMatrix;
+}
+
 
 // TODO: Copy the matrices and return a new instance of the matrix
 function MatrixGenerator(size){
@@ -264,29 +281,12 @@ function renderProperties(matrix){
         return html;
     }
 
-    html = displayMessage("Identity", isIdentity(matrix), html);
-    html = displayMessage("Orthogonal", isOrthogonal(matrix), html);
-    html = displayMessage("Symmetric", isSymmetric(matrix), html);
+    html = displayMessage("Identity", matrix.isIdentity(), html);
+//    html = displayMessage("Orthogonal", isOrthogonal(matrix), html);
+    html = displayMessage("Symmetric", matrix.isSymmetric(), html);
 
     html += "</div>";
     return html;
-}
-
-function parseMatrix(input){
-    var matrix = [];
-    input.split('\n').each(function(line){
-        line = line.compact();
-        // Ignore any empty new lines
-        if(line.length == 0) return;
-        var row = [];
-        line.split(' ').each(function(el){
-            // Allow real numbers (and even complex numbers?)
-            row.push(parseInt(el)); 
-        });
-        matrix.push(row);
-    });
-    renderMatrix(matrix);
-    return matrix;
 }
 
 renderApp();
