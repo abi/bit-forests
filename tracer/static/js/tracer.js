a900b65b749639a3335a68f41e33171c3f7c4db7
Abi Raja <abimanyuraja@gmail.com>
2013-02-22 22:54:33 -0800

    Fixed a type bug that was causing lines to be offset wrongly.

--- tracer/static/js/tracer.js
+++ tracer/static/js/tracer.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var socket = io.connect('http://localhost');
+    var socket = io.connect('http://localhost');
     socket.on('update', function (data) {
       console.log("UPDATING");
       $('#source').html(data['source'].replace(/\n/gi, '<br/>'));
@@ -11,34 +11,37 @@ $(document).ready(function(){
 
 var traces = {};
 function trace(name, value, line){
-  if(!Object.has(traces, line)) traces[line] = [];
-  traces[line].push({'name' : name, 'value' : value, 'line' : line});
+    line = parseInt(line);
+    if(!Object.has(traces, line)) traces[line] = [];
+    traces[line].push({'name' : name, 'value' : value, 'line' : line});
 }
 
 function renderTrace(){
-  var lines = Object.keys(traces).sortBy();
-  var html = "";
-  var prevLine = 0;
-  for(var i = 0; i < lines.length; i++){
+    // Convert all keys to strings
+    var lines = Object.keys(traces).map(function(i){return parseInt(i);}).sortBy();
+    var html = "";
+    var prevLine = 0;
+    for(var i = 0; i < lines.length; i++){
     var line = lines[i];
     var traceList = traces[line];
 
-    var linesSkipped = parseInt(line) - prevLine ;
+    var linesSkipped = parseInt(line) - prevLine;
+
     if(linesSkipped > 1){
-    	for(var k = 0; k < linesSkipped; k++)
-	    	html += "<br/>";
+        for(var k = 0; k < linesSkipped; k++)
+            html += "<br/>";
     }
 
     for(var j = 0; j < traceList.length; j++){
       var trace = traceList[j];
       html += trace['name'] + " = " + trace['value'];
       if(j != traceList.length - 1){
-      	html += " | ";
+        html += " | ";
       }
     }
     html += "<br/>";
     prevLine = parseInt(line);
-  }
-  console.log(html);
-  return html;
+    }
+    console.log(html);
+    return html;
 }
