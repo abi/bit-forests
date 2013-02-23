2f2bc1c9386fc1723791295b1336ea5e3455dcd2
Abi Raja <abimanyuraja@gmail.com>
2013-02-23 12:46:56 -0800

    Created a audio project, based off Apple's example code to play with Web Audio. Modified it to use a sine wave osc as the source.

---
+++ audio/server.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.end(fs.readFileSync('audio.html', 'utf8'));
+});
+
+app.use(express.static(__dirname + '/static'));
+app.listen(3000);
