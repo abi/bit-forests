1ef1f5599a627b854a74c2f2acab6d3a935ae577
Abi Raja <abimanyuraja@gmail.com>
2013-03-14 22:54:34 -0700

    Monte Carlo Integration.

--- we-might-never-die/experiments.js
+++ we-might-never-die/experiments.js
@@ -3,3 +3,17 @@ function greeter(person) {
 }
 var user = "Test";
 console.log(greeter(user));
+function integrate() {
+    var NUM_POINTS = 1000000;
+    var numBelowFn = 0;
+    for(var i = 0; i < NUM_POINTS; i++) {
+        var x = Math.random() * 2.0;
+        var y = Math.random() * Math.exp(2.0);
+        if(y < Math.exp(x)) {
+            numBelowFn++;
+        }
+    }
+    var fraction = numBelowFn / NUM_POINTS;
+    return 2 * Math.exp(2) * fraction;
+}
+console.log(integrate());
--- we-might-never-die/experiments.ts
+++ we-might-never-die/experiments.ts
@@ -1,7 +1,16 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+
+// Monte Carlo Integration to integrate e^x from 0 to 2.
+// Based on CS109 Lecture Notes
+function integrate(){
+	var NUM_POINTS = 1000000;
+	var numBelowFn = 0;
+	for(var i = 0; i < NUM_POINTS; i++){
+		var x : number = Math.random() * 2.0;
+		var y : number = Math.random() * Math.exp(2.0);
+		if(y < Math.exp(x)) numBelowFn++;
+	}
+	var fraction: number = numBelowFn / NUM_POINTS;
+	return 2 * Math.exp(2) * fraction;
 }
 
-var user = "Test";
-
-console.log(greeter(user));
+console.log(integrate());
