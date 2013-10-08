e964d97a2ac6c02986d191fef237d7c018a7d6f7
Abi Raja <abimanyuraja@gmail.com>
2013-10-07 18:42:54 -0700

    Store chronological traces.

--- tracer/static/js/tracer.js
+++ tracer/static/js/tracer.js
@@ -13,40 +13,51 @@ $(document).ready(function () {
 function Tracer () {
   var self = this
   self.lineTraces = {}
+  self.traces = []
 }
 
 Tracer.prototype.getType_ = function (obj) {
   return Object.prototype.toString.call(obj).slice(8, -1)
 }
 
-Tracer.prototype.trace = function (name, value, line) {
+Tracer.prototype.storeTrace_ = function (trace) {
   var self = this
 
+  // Store a chronological list of traces
+  self.traces.push(trace)
+
+  // Store all traces associated with a line
+  var line = trace.line
   if (!Object.has(self.lineTraces, line)) {
     self.lineTraces[line] = []
   }
 
-  // Note: JSON.stringify makes the value human-readable
-  self.lineTraces[line].push({
+  self.lineTraces[line].push(trace)
+}
+
+Tracer.prototype.trace = function (name, value, line) {
+  var self = this
+
+  // NOTE: JSON.stringify makes the value human-readable
+  var trace = {
     'name' : name,
     'value' : JSON.stringify(value),
     'line' : line
-  })
+  }
+
+  self.storeTrace_(trace)
 }
 
 Tracer.prototype.traceCall = function (name, obj, method, line) {
   var self = this
 
-  if (!Object.has(self.lineTraces, line)) {
-    self.lineTraces[line] = []
-  }
-
   if (self.getType_(obj) === 'Array') {
-    self.lineTraces[line].push({
+    var trace = {
       'name' : name,
       'value' : JSON.stringify(obj),
       'line' : line
-    })
+    }
+    self.storeTrace_(trace)
   }
 }
 
@@ -67,9 +78,9 @@ Tracer.prototype.render = function () {
     var linesSkipped = parseInt(line) - prevLine
 
     if(linesSkipped > 1){
-        for (var k = 0; k < linesSkipped; k++) {
-            html += "<br/>"
-        }
+      for (var k = 0; k < linesSkipped; k++) {
+          html += "<br/>"
+      }
     }
 
     for (var j = 0; j < traceList.length; j++) {
